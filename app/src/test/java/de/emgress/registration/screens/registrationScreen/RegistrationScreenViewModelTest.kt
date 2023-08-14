edae3d5a2c2da888bacfa1796613ff502c8676a2
Dmitry Chervontsev <chervontsev@emgress.de>
2023-08-14 13:19:30 +0200

    Commit 16 - UnitTest für ViewModel angelegt

---
+++ app/src/test/java/de/emgress/registration/screens/registrationScreen/RegistrationScreenViewModelTest.kt
@@ -0,0 +1,21 @@
+package de.emgress.registration.screens.registrationScreen
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class RegistrationScreenViewModelTest {
+
+    lateinit var mUUT: RegistrationScreenViewModel
+
+    @Before
+    fun setUp() {
+        mUUT = RegistrationScreenViewModel()
+    }
+
+    @Test
+    fun checkFormularIsReady(){
+
+    }
+}
