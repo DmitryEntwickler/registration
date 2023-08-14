c8784fe1e053000775931fb0773963f60a1d942b
Dmitry Chervontsev <chervontsev@emgress.de>
2023-08-14 13:08:25 +0200

    Commit 14 - Home Screen UI angepasst

--- app/src/main/java/de/emgress/registration/screens/homeScreen/HomeScreenComposable.kt
+++ app/src/main/java/de/emgress/registration/screens/homeScreen/HomeScreenComposable.kt
@@ -1,15 +1,32 @@
 package de.emgress.registration.screens.homeScreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import de.emgress.registration.R
 
 @Composable
 fun HomeScreenComposable(){
 
-    Column() {
-        Text(text = stringResource(id = R.string.welcome))
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text(
+            text = stringResource(id = R.string.welcome),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            textAlign = TextAlign.Center
+        )
     }
 }
