14b8b91a7e48bf0c7757596b441e917b6582d32e
Dmitry Chervontsev <chervontsev@emgress.de>
2023-08-14 10:28:47 +0200

    Commit 4 - TopAppBar

---
+++ app/src/main/java/de/emgress/registration/commonComposables/TopAppBarComposable.kt
@@ -0,0 +1,26 @@
+package de.emgress.registration.commonComposables
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import de.emgress.registration.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarComposable(){
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(id = R.string.topAppBarText),
+                color = MaterialTheme.colorScheme.onPrimary
+            ) },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
--- app/src/main/java/de/emgress/registration/navigation/NavHostComposable.kt
+++ app/src/main/java/de/emgress/registration/navigation/NavHostComposable.kt
@@ -3,13 +3,17 @@ package de.emgress.registration.navigation
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import de.emgress.registration.R
+import de.emgress.registration.commonComposables.TopAppBarComposable
 import de.emgress.registration.screens.registrationScreen.RegistrationScreenComposable
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,7 +23,7 @@ fun NavHostComposable(){
     val mNavController = rememberNavController()
 
     Scaffold(
-        bottomBar = { }
+        topBar = { TopAppBarComposable()}
     ) { innerPaddingValues ->
         NavHost(
             navController = mNavController,
